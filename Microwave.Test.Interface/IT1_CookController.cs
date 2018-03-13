620d27d9cfffb29d153d615ce1891ceedc6fea67
Jonas Kjær Rask <jonas.krask@gmail.com>
2018-03-13 10:21:27 +0100

    setup done

--- Microwave.Test.Interface/IT1_CookController.cs
+++ Microwave.Test.Interface/IT1_CookController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Interface
@@ -10,5 +14,26 @@ namespace Microwave.Test.Interface
     [TestFixture]
     public class IT1_CookController
     {
+        private IOutput _output;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        private ILight _light;
+        private IDisplay _display;
+        private ICookController _cookController;
+        private IUserInterface _userInterface;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = Substitute.For<Output>();
+            _userInterface = Substitute.For<UserInterface>();
+            _timer = new Timer();
+            _powerTube = new PowerTube(_output);
+            _light = new Light(_output);
+            _display = new Display(_output);
+            _cookController = new CookController(_timer,_display,_powerTube, _userInterface);
+        }
+
+
     }
 }
